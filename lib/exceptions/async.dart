18d6b33ca131184099fc5390a523b7be8a20cc99
Tommy Stigen Olsen <tommysolsen@gmail.com>
2023-03-21 15:55:26 +0100

    Add async fold function

---
+++ lib/exceptions/async.dart
@@ -0,0 +1,9 @@
+extension AsyncFold<T, C> on Iterable<T> {
+  Future<C> foldAsync(C initialValue, Future<C> Function(C, T) predicate) async {
+    var cur = initialValue;
+    for(var item in this) {
+      cur = await predicate(cur, item);
+    }
+    return cur;
+  }
+}
